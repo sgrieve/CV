1a749ffc2a59560858446ebb179ea82dffeaf610
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-12-20 15:07:18 +0000

    tweaks to the build script to make things more tidy

--- build.sh
+++ build.sh
@@ -6,9 +6,15 @@
 if [ "$1" = "short" ]
 then
   cp CV.tex tmp.tex
+  # Build twice so cross referencing is correct.
+  xelatex "\newif\iflong \input{tmp.tex}"
   xelatex "\newif\iflong \input{tmp.tex}"
   mv tmp.pdf CV_short.pdf
   rm tmp.*
 else
+  # Build twice so cross referencing is correct.
   xelatex "\newif\iflong \longtrue \input{CV.tex}"
+  xelatex "\newif\iflong \longtrue \input{CV.tex}"
+  # Clean up the temp files
+  rm CV.log CV.aux CV.out
 fi
